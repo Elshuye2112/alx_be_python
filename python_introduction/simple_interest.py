d434737926b3217d0a70f34fd4ceb148613f5e85
Elshaday021 <elshaday.basie@berhanbanksc.com>
2025-05-27 00:45:51 -0700

    The second commit has been done

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# simple_interest.py
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
+
+name="Elshaday Basie"
+
+print(f"The second task has been done by {name} ")
