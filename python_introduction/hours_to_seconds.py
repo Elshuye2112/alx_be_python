7b6409bf27c78dd088e9d099fddb9ddc61a53dae
Elshaday021 <elshaday.basie@berhanbanksc.com>
2025-05-27 01:02:32 -0700

    hours to minutes and seconds conversion feature has been added.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+#hours_to_seconds.py
+
+hours=2
+seconds=hours*3600
+
+minutes=hours*60
+
+print(f"{hours} hours is equal to {seconds} seconds")
+
+print(f"{hours} hours is equal to {minutes} minutes")
