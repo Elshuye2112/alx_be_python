3c5c93bfe0d2bb6cd50e0e34f4f04bb743c54e5f
Elshaday021 <elshaday.basie@berhanbanksc.com>
2025-05-27 00:55:49 -0700

    Calculating the area and permeter of the rectangle feature added.

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,13 @@
+#rectangle_area.py
+
+length=10
+width=5
+
+
+area=length*width
+
+permeter=2*(length+width)
+
+print(f"The Area of the Rectangle is: {area}")
+
+print(f"The permeter of the rectangle is: {permeter}")
