6654e9e4d3bbcf5d47aaa2a094114ebc576a008f
Elshuye2112 <basie.elsh2112@gmail.com>
2025-06-17 14:11:23 +0300

    str created

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,32 +1,37 @@
-# Base class
 class Book:
     def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
-# Derived class - EBook
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size  # in KB
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
-# Derived class - PrintBook
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Composition class - Library
 class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)  # __str__ is called here automatically
